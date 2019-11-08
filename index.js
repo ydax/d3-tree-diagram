bec562a58dcca00d943d74a34f3d1698dc532ed7
pdavisjones <pdavisjones@gmail.com>
2019-11-08 17:46:13 -0600

    added front-end and form

---
+++ index.js
@@ -0,0 +1,4 @@
+// grab the modal div class
+const modal = document.querySelector('.modal');
+// M = materliaze library object | Modal.init() method launches the referenced modal
+M.Modal.init(modal);
