6e7a4c7bc9646e9a4d64a042d5093d8cabd5320a
pdavisjones <pdavisjones@gmail.com>
2019-11-10 15:13:31 -0600

    built 90% of graph interactivity

--- graph.js
+++ graph.js
@@ -1,3 +1,86 @@
+// setup the dimensions of the tree diagram
+const dims = { height: 500, width: 1100 };
+
+const svg = d3.select('.canvas')
+    .append('svg')
+    .attr('width', dims.width + 100)
+    .attr('height', dims.height + 100);
+
+const graph = svg.append('g')
+    .attr('transform', 'translate(50, 50)');
+
+// setup a data stratification function 
+const stratify = d3.stratify()
+    .id(d => d.name)
+    .parentId(d => d.parent);
+
+// fcn for turning stratified data into a tree diagram
+const tree = d3.tree()
+    .size([dims.width, dims.height]);
+
+// update function
+const update = (data) => {
+    // remove current nodes
+    graph.selectAll('.node').remove();
+    graph.selectAll('.link').remove();
+    
+    // store data in stratified format in rootNode
+    const rootNode = stratify(data);
+    
+    // pass stratified data into the d3 tree fcn
+    const treeData = tree(rootNode);
+    
+    // append anything with a class of node to the graph
+    const nodes = graph.selectAll('.node')
+        // chain the descendants method on so that the treeData returns in array format (needed by d3)
+        .data(treeData.descendants());
+        // ^-- join data to anything with the class "node"
+    
+    // get link selection and join data
+    const links = graph.selectAll('.link')
+        .data(treeData.links());
+    //                   ^-- dynamically generate paths between nodes
+
+    links.enter()
+        .append('path')
+        .attr('class', 'link')
+        .attr('fill', 'none')
+        .attr('stroke', '#666666')
+        .attr('stroke-width', 2)
+        .attr('d', d3.linkVertical()
+            .x(d => d.x)
+            .y(d => d.y)
+        );
+    
+    // create enter node groups
+    const enterNodes = nodes.enter()
+        //                        ^-- get the enter selection from nodes
+        .append('g')
+        //    ^-- append a group to each enter selection
+            .attr('class', 'node')
+        //               ^-- give each group the class "node"
+            .attr('transform', d => `translate(${ d.x }, ${ d.y })`);
+    
+    // append rects to enter nodes
+    enterNodes.append('rect')
+        .attr('fill', '#000099')
+        .attr('stroke', '#ff3366')
+        .attr('stroke-width', 2)
+        .attr('height', 50)
+        .attr('width', d => { d.data.name.length * 20 })
+        //           ^-- dynamically generate width based on length of name
+        .attr('transform', d => {
+            let xShift = d.data.name.length * 10;
+            return `translate(${-xShift}, -25)`;
+        });
+    
+    // append text to the rectangles
+    enterNodes.append('text')
+        .attr('text-anchor', 'middle')
+        .attr('fill', '#ffff33')
+        .text(d => d.data.name);
+}
+
 var data = [];
 
 // grab the database and the employees collection
@@ -31,6 +114,6 @@ db.collection('employees').onSnapshot(res => {
         }
     });
 
-    console.log(data);
-    
+    update(data);
+
 });
